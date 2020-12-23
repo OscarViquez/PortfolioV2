2d73e8bf7683e8b20b74eae3ea7e72da52c1cd48
Oscar H.V <55415043+OscarViquez@users.noreply.github.com>
2020-12-22 23:02:40 -0500

    added styling to sections: Intro, Title, ProjectCards. made them responsive using media queries. added another set of fonts to improve UI. sit is mostlty responsive in mobile an tablet, not desktop yet

--- js/index.js
+++ js/index.js
@@ -24,4 +24,16 @@ $(function () {
     });
   });
 
+
+//   $(document).ready(function(){       
+//     let scroll_pos = 0;
+//     $(document).scroll(function() { 
+//         scroll_pos = $(this).scrollTop();
+//         if(scroll_pos > 410) {
+//             $('.menu-item').css('color', '#000');
+//         } else {
+//             $('.menu-item').css('color', '#fff');
+//         }
+//     });
+// });
   
